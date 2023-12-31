b0497506590c50f5a170cf3c0405c491df223485
FreshDev <billyhans90@gmail.com>
2023-12-31 02:09:33 +0100

    Refactor the entire code

--- routes/genres.js
+++ routes/genres.js
@@ -1,56 +1,63 @@
-const { Genre, validate } = require('./models/genre')
-const express = require('express')
-const router = express.Router()
+const { Genre, validate } = require("./models/genre");
+const express = require("express");
+const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const genres = await Genre.find().sort('name')
-    res.send(genres)
-})
+router.get("/", async (req, res) => {
+  const genres = await Genre.find().sort("name");
+  res.send(genres);
+});
 
-router.post('/', async (req, res) => {
-    const {error} = validate(req.body)
-    if(error) {
-        return res.status(400).send(error.details[0].message)
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  let genre = new Genre({
+    // id: genres.length + 1,
+    name: req.body.name,
+  });
+  genre = await genre.save();
+  // try {
+  //     const genre = await genre.save()
+  //     console.log(genre)
+  // } catch(exception) {
+  //     console.log(exception.message)
+  // }
+  res.send(genre);
+});
+
+router.get("/:id", async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+  res.send(genre);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true, //This is so that we get the updated object from the database
     }
-    let genre = new Genre({
-        // id: genres.length + 1,
-        name: req.body.name
-    })
-    genre = await genre.save()
-    // try {
-    //     const genre = await genre.save()
-    //     console.log(genre)
-    // } catch(exception) {
-    //     console.log(exception.message)
-    // }
-    res.send(genre)
-})
+  );
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
 
-router.get('/:id', async (req, res) => {
-    const genre = await Genre.findById(req.params.id)
-    if(!genre) return res.status(404).send('The genre with the given ID was not found.')
-    res.send(genre)
-})
+  res.send(genre);
+});
 
-router.put('/:id', async (req, res) => {
-    const {error} = validate(req.body)
-    if(error) {
-        return res.status(400).send(error.details[0].message)
-    }
+router.delete("/:id", async (req, res) => {
+  const genre = await Genre.findByIdAndDelete(req.params.id);
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
 
-    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name}, {
-        new: true //This is so that we get the updated object from the database
-    })
-    if(!genre) return res.status(404).send('The genre with the given ID was not found.')
+  res.send(genre);
+});
 
-    res.send(genre)
-})
-
-router.delete('/:id', async (req, res) => {
-    const genre = await Genre.findByIdAndDelete(req.params.id)
-    if(!genre) return res.status(404).send('The genre with the given ID was not found.')
-
-    res.send(genre)
-})
-
-module.exports = router
+module.exports = router;
--- routes/models/genre.js
+++ routes/models/genre.js
@@ -1,25 +1,23 @@
-const Joi = require('joi')
-const mongoose = require('mongoose')
-
+const Joi = require("joi");
+const mongoose = require("mongoose");
 
 const genreSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        minLength: 5,
-        maxLength: 50,
-    }
-})
+  name: {
+    type: String,
+    required: true,
+    minLength: 5,
+    maxLength: 50,
+  },
+});
 
-const Genre = mongoose.model('Genre', genreSchema)
-
+const Genre = mongoose.model("Genre", genreSchema);
 
 function validateGenre(genre) {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
-    return schema.validate(genre)
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
 }
 
-exports.Genre = Genre
-exports.validate = validateGenre
+exports.Genre = Genre;
+exports.validate = validateGenre;
