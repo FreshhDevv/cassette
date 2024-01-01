5f3a66e8726220340b8558ccbdd16c126f8a9acf
FreshDev <billyhans90@gmail.com>
2024-01-01 15:55:19 +0100

    Rental model done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const genres = require("./routes/genres");
 const customers = require('./routes/customers')
 const movies = require('./routes/movies')
+const rentals = require('./routes/rentals')
 
 mongoose
   .connect(process.env.MONGO_DB)
@@ -18,6 +19,7 @@ app.use("/api/genres", genres);
 // Anywhere you see 'api/customers' it should match it up to the customers module loaded above.
 app.use('/api/customers', customers)
 app.use('/api/movies', movies)
+app.use('/api/rentals', rentals)
 
 // PORT
 const port = process.env.PORT || 3000;
