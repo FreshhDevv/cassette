ee36abccc5849cff68be5492f965ab3f47d839d3
FreshDev <billyhans90@gmail.com>
2024-01-02 13:10:56 +0100

    Better implementation for objectId

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
-
+const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi)
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const genres = require("./routes/genres");
