391ddc88176ad1160d4ca238154842c8cec86eb8
FreshDev <billyhans90@gmail.com>
2024-01-03 22:05:52 +0100

    Use lodash for registration

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const _ = require('lodash')
 const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
@@ -13,16 +14,18 @@ router.post("/", async (req, res) => {
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
-  user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  // user = new User({
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password,
+  // });
   await user.save();
-  res.send({
-    name: user.name,
-    email: user.email,
-  });
+  res.send(  _.pick(user, ['_id', 'name', 'email'])  );
+  // res.send({
+  //   name: user.name,
+  //   email: user.email,
+  // });
 });
 
 router.get("/:id", async (req, res) => {
