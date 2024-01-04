c92cfceeecc457aeacb1f4b6a1e0b111e9ae8a13
FreshDev <billyhans90@gmail.com>
2024-01-04 08:56:24 +0100

    Setting response headers

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-const _ = require('lodash')
+const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt')
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 router.get("/", async (req, res) => {
   const users = await User.find();
@@ -16,16 +17,20 @@ router.post("/", async (req, res) => {
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered.");
-  user = new User(_.pick(req.body, ['name', 'email', 'password']));
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
   // user = new User({
   //   name: req.body.name,
   //   email: req.body.email,
   //   password: req.body.password,
   // });
-  const salt = await bcrypt.genSalt(10)
-  user.password = await bcrypt.hash(user.password, salt)
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
-  res.send(  _.pick(user, ['_id', 'name', 'email'])  );
+  const token = jwt.sign({ _id: user._id }, config.get("jwtPrivateKey"));
+  res
+    .header("x-auth-token", token)
+    .send(_.pick(user, ["_id", "name", "email"]));
+  // res.send(  _.pick(user, ['_id', 'name', 'email'])  );
   // res.send({
   //   name: user.name,
   //   email: user.email,
