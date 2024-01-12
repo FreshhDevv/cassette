38f04bad4e7e1ae04d268de3c6a22c078bdc8ac6
FreshDev <billyhans90@gmail.com>
2024-01-12 08:33:56 +0100

    Clean code

--- middleware/error.js
+++ middleware/error.js
@@ -1,7 +1,7 @@
 function error(err, req, res, next) {
-  const logger = require('..'); // Move the import inside the function
-  logger.error(err.message, err );
-  res.status(500).send('Something failed.');
+  const logger = require(".."); // Move the import inside the function
+  logger.error(err.message, err);
+  res.status(500).send("Something failed.");
 }
 
 module.exports = error;
--- routes/genres.js
+++ routes/genres.js
@@ -4,82 +4,63 @@ const { Genre, validate } = require("../models/genre");
 const express = require("express");
 const router = express.Router();
 
-router.get(
-  "/",
-  async (req, res) => {
-    throw new Error('Could not get the genres.')
-    const genres = await Genre.find().sort("name");
-    res.send(genres);
-  }
-);
+router.get("/", async (req, res) => {
+  throw new Error("Could not get the genres.");
+  const genres = await Genre.find().sort("name");
+  res.send(genres);
+});
 
-router.post(
-  "/",
-  auth,
-  async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) {
-      return res.status(400).send(error.details[0].message);
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  const genre = new Genre({
+    // id: genres.length + 1,
+    name: req.body.name,
+  });
+  await genre.save();
+  // try {
+  //     const genre = await genre.save()
+  //     console.log(genre)
+  // } catch(exception) {
+  //     console.log(exception.message)
+  // }
+  res.send(genre);
+});
+
+router.get("/:id", auth, async (req, res) => {
+  const genre = await Genre.findById(req.params.id);
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+  res.send(genre);
+});
+
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  const genre = await Genre.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    {
+      new: true, //This is so that we get the updated object from the database
     }
-    const genre = new Genre({
-      // id: genres.length + 1,
-      name: req.body.name,
-    });
-    await genre.save();
-    // try {
-    //     const genre = await genre.save()
-    //     console.log(genre)
-    // } catch(exception) {
-    //     console.log(exception.message)
-    // }
-    res.send(genre);
-  }
-);
+  );
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
 
-router.get(
-  "/:id",
-  auth,
-  async (req, res) => {
-    const genre = await Genre.findById(req.params.id);
-    if (!genre)
-      return res.status(404).send("The genre with the given ID was not found.");
-    res.send(genre);
-  }
-);
+  res.send(genre);
+});
 
-router.put(
-  "/:id",
-  auth,
-  async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) {
-      return res.status(400).send(error.details[0].message);
-    }
+router.delete("/:id", [auth, admin], async (req, res) => {
+  const genre = await Genre.findByIdAndDelete(req.params.id);
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
 
-    const genre = await Genre.findByIdAndUpdate(
-      req.params.id,
-      { name: req.body.name },
-      {
-        new: true, //This is so that we get the updated object from the database
-      }
-    );
-    if (!genre)
-      return res.status(404).send("The genre with the given ID was not found.");
-
-    res.send(genre);
-  }
-);
-
-router.delete(
-  "/:id",
-  [auth, admin],
-  async (req, res) => {
-    const genre = await Genre.findByIdAndDelete(req.params.id);
-    if (!genre)
-      return res.status(404).send("The genre with the given ID was not found.");
-
-    res.send(genre);
-  }
-);
+  res.send(genre);
+});
 
 module.exports = router;
