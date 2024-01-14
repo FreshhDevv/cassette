5c3f3c075ff665bc4c1d6a7729f4ac95a7e76f8d
FreshDev <billyhans90@gmail.com>
2024-01-14 12:25:00 +0100

    Unhandled promise rejections

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ async function connectToMongoDB() {
 
 connectToMongoDB();
 
-process.on('uncaughtException', (ex) => {
-  console.log('WE GOT AND UNCAUGHT EXCEPTION')
-  logger.error(ex.message, ex)
-})
+// process.on('uncaughtException', (ex) => {
+//   logger.error(ex.message, ex)
+//   process.exit(1)
+// })
 
 const logger = winston.createLogger({
   level: "error",
@@ -47,7 +47,25 @@ const logger = winston.createLogger({
   ],
 });
 
-throw new Error('Something failed during startup.')
+// winston.exceptions.handle(new winston.transports.File({filename: 'uncaughtExeptions.log'}))
+logger.exceptions.handle(new winston.transports.File({filename: 'uncaughtExeptions.log'}))
+
+
+// throw new Error('Something failed on startup.')
+
+// process.on('unhandledRejection', (ex) => {
+//   logger.error(ex.message, ex)
+//   process.exit(1)
+// })
+
+logger.rejections.handle(new winston.transports.File({filename: 'uncaughtRejections.log'}))
+
+throw new Error('Something failed miserably.')
+
+
+
+// const p = Promise.reject(new Error('Something failed miserably.'))
+// p.then(() => console.log('Done'))
 
 logger.info("Server started successfully.");
 
