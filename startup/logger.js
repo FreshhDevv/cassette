3de76da4ff30d4d4c06a3416f35a850c83fc7038
FreshDev <billyhans90@gmail.com>
2024-01-21 09:25:54 +0100

    Show unhandled exceptions on the console

--- startup/logger.js
+++ startup/logger.js
@@ -3,14 +3,18 @@ require("express-async-errors");
 require("winston-mongodb");
 
 const logger = winston.createLogger({
-    level: ["error","info"],
-    format: winston.format.json(),
-    transports: [
-        new winston.transports.File({ filename: "logfile.log" }),
-        new winston.transports.MongoDB({ db: process.env.MONGO_DB, level: 'error' }),
-        new winston.transports.File({ filename: "uncaughtExceptions.log" }),
-        new winston.transports.File({ filename: "uncaughtRejections.log" }),
-    ],
+  level: ["error", "info"], // or whichever level you prefer
+  format: winston.format.json(),
+  transports: [
+      new winston.transports.File({ filename: "logfile.log" }),
+      new winston.transports.MongoDB({ db: process.env.MONGO_DB, level: ['error', 'info'] }),
+      new winston.transports.File({ filename: "uncaughtExceptions.log" }),
+      new winston.transports.File({ filename: "uncaughtRejections.log" }),
+      new winston.transports.Console({ 
+          format: winston.format.simple(), // Specify the format for console output
+      }),
+  ],
 });
+
 
 module.exports = logger;
