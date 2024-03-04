d97aa3dd34eb14a08665dfe11564d4a6b3ead0bd
FreshDev <billyhans90@gmail.com>
2024-03-04 05:26:01 +0100

    Solved logging errors

--- startup/db.js
+++ startup/db.js
@@ -1,25 +1,18 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
-
-const dbLogger = winston.createLogger({
-  level: "error",
-  format: winston.format.json(),
-  transports: [
-    new winston.transports.File({filename: "logfile.log"})
-  ]
-})
+const logger = require("./logger"); // Import the logger module
 
 async function connectToMongoDB() {
-    try {
-        await mongoose.connect(process.env.MONGO_DB);
-        // logger.info('Connected to MongoDB...')
-        dbLogger.info('Connected to MongoDB...')
-    } catch (err) {
-      dbLogger.error(err.message, err);
-      process.exit(1); // Exit the application if MongoDB connection fails
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    // logger.info('Connected to MongoDB...')
+    logger.info("Connected to MongoDB...");
+  } catch (err) {
+    logger.error(err.message, err);
+    process.exit(1); // Exit the application if MongoDB connection fails
   }
-  
-  connectToMongoDB();
+}
 
-  module.exports = connectToMongoDB
+connectToMongoDB();
+
+module.exports = connectToMongoDB;
